138d0ea7389d597392a13a01c8458fb2f2bb5057
ranxiong <390630235@qq.com>
2018-11-01 16:05:24 +0800

    first commit

---
+++ src/test/java/com/example/redislock/RedisCheckLockTest.java
@@ -0,0 +1,69 @@
+package com.example.redislock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisCheckLockTest {
+    private static Logger logger = LoggerFactory.getLogger(RedisCheckLock.class);
+    @Autowired
+    RedisCheckLock redisCheckLock;
+
+    @Test
+    public void test() {
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+        for (int i = 1; i <= 100; i++) {
+            executorService.submit(() -> {
+                this.doBussiness(Thread.currentThread().getName());
+            });
+        }
+        // main thread block here
+        try {
+            System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void doBussiness(String requestId) {
+        String key = "REDIS:CHECK:LOCK:" + UUID.randomUUID().toString();
+        int expires = 10000;
+        while (true) {
+            Boolean aBoolean = redisCheckLock.tryLock(key, requestId, expires);
+            if (aBoolean) {
+                break;
+            } else {
+                // 尝试重新获取锁
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        logger.info("Thead[{}]获得锁,key:{}", requestId, key);
+        // 模拟处理业务时间
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            logger.info("Thead[{}]释放锁,key:{}", requestId, key);
+            redisCheckLock.releaseLock(key, requestId);
+        }
+
+    }
+}
